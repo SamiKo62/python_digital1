be6e82c965150d2b4dba7acb1e27cf70d4370ea7
Sam <sameli57@gmail.com>
2022-01-02 10:24:43 +0200

    test1 program

---
+++ Lesson1/Trgil4/Targil4.py
@@ -0,0 +1,13 @@
+
+a='''
+full name: Shmuel Elimelech
+Age: 50
+Email:same@shani.net
+'''
+print(a)
+b="Shmuel Elimelech"
+c=50*3
+print("Oposite name:" + b [::-1] + "\nAge times3 :" + str(c))
+print("Shmuel" in "Idan,Ben,Dudu,Yuval,Shimon,Yael,Gal,Adam,Shahar,Yona")
+
+
