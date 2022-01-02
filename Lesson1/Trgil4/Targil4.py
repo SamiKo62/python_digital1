65128974334a291393324a94ba42a820e6d31de2
Sam <sameli57@gmail.com>
2022-01-02 10:46:18 +0200

    test1 program

--- Lesson1/Trgil4/Targil4.py
+++ Lesson1/Trgil4/Targil4.py
@@ -1,13 +1,9 @@
 
-a='''
-full name: Shmuel Elimelech
-Age: 50
-Email:same@shani.net
-'''
-print(a)
-b="Shmuel Elimelech"
-c=50*3
-print("Oposite name:" + b [::-1] + "\nAge times3 :" + str(c))
+a="Shmuel Elimelelech"
+b=50
+c="same@shani.net"
+print("Full name" + a + "\nAge: " + str(b) +"\nEmail:" + c)
+print("Oposite name:" + a [::-1] + "\nAge times3 :" + str(b*3))
 print("Shmuel" in "Idan,Ben,Dudu,Yuval,Shimon,Yael,Gal,Adam,Shahar,Yona")
 
 
