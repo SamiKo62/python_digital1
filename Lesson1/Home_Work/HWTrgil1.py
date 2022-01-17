a0f300a1a858fdf8ce677669eda1ef42d31d213a
Sam <sameli57@gmail.com>
2022-01-17 19:09:59 +0200

    Lab2

---
+++ Lesson1/Home_Work/HWTrgil1.py
@@ -0,0 +1,6 @@
+#Write a Python program to print the following string in a specific format
+# (see the output).
+# Sample String : "Net4U, is the best place…in the world" Output :
+# "Net4U, is the best place
+#       …in the world"
+print("Net4u, Is the best place\n......in the world")
