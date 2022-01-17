93d766e6e78458bf74de73b16da3f857217027a5
Sam <sameli57@gmail.com>
2022-01-17 21:16:10 +0200

    Lab2

---
+++ Lesson1/Home_Work/HWTargil3/HWTargil3.py
@@ -0,0 +1,7 @@
+#Write a Python program which accepts the user's first and last name
+# and print them in reverse order with a space between them.
+a=input("Enter full name")
+print(a)
+print(''.join(a[::-1]))
+
+
