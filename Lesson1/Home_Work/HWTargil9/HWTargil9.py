fe2b225eaef5efb9c881e2354e4097c10c1b5787
Sam <sameli57@gmail.com>
2022-01-25 22:04:55 +0200

    Lab2

---
+++ Lesson1/Home_Work/HWTargil9/HWTargil9.py
@@ -0,0 +1,6 @@
+#Write a Python script to add a key to a dictionary.
+#Sample Dictionary : {0: 10, 1: 20}
+#expected Result : {0: 10, 1: 20, 2: 30}
+my_dict={0:10,1:20}
+my_dict[2]=30
+print(my_dict)
