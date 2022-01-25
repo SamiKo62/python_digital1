10d31445069234e7f0581dda75f52ecf36b93373
Sam <sameli57@gmail.com>
2022-01-25 21:47:23 +0200

    Lab2

---
+++ Lesson1/Home_Work/HWTargil8/HWTargil8.py
@@ -0,0 +1,12 @@
+#Write a Python program to empty a variable without destroying it.
+#Sample data: n=20
+#d = {"x":200}
+#Expected Output : 0
+#{}
+n=20
+d={"x":200}
+l=[2,4,6]
+#print(n)
+print(type(n)())
+print(type(d)())
+print(type(l)())
